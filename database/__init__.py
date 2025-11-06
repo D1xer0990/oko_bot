858de2f7c5886c57a1363d9210285ed3692ea26e
D1xer0990 <87071907+D1xer0990@users.noreply.github.com>
2025-11-06 22:52:55 +0300

    бекап

--- database/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Модуль для работы с базой данных"""
-from .models import Base, Person, UserLog
-from .database import Database, get_db_session
-
-__all__ = ['Base', 'Person', 'UserLog', 'Database', 'get_db_session']
-
